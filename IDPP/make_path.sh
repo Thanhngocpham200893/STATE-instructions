f73e674d7119a7c856a9dd5d90837379caa12df9
Thanhngocpham200893 <68117496+Thanhngocpham200893@users.noreply.github.com>
2021-07-22 18:28:01 +0900

    Create make_path.sh

---
+++ IDPP/make_path.sh
@@ -0,0 +1,31 @@
+###This script is use to generate the nfinp and nugded_2 from xyz file 
+
+#! /bin/bash
+
+for i in {2..6}
+do
+
+mkdir n$i
+cd n$i
+cp ../image"$i".xyz old.xyz
+cp ../xyz2nfinp.py .
+cp ../index.txt .
+cp ../state2nudged_2.py .
+
+cat ../nfinp_ini > nfinp
+python3 xyz2nfinp.py old.xyz > cps.dat
+paste cps.dat index.txt >> nfinp
+cat ../nfinp_end >> nfinp
+python3 state2nudged_2.py nfinp > nudged_2 
+cp nfinp  nfinp_1
+state2xyz.py nfinp_1 > nfinp_1.xyz
+
+let e=$i-1
+let f=$i+1
+ln -fs ../n$e/nudged_2 nudged_1
+ln -fs ../n$f/nudged_2 nudged_3
+
+
+cd ../
+
+done
