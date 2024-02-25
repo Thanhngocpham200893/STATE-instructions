c66761d2bedf3b6774b3f6cfc8a4e659ff37189e
Thanhngocpham200893 <68117496+Thanhngocpham200893@users.noreply.github.com>
2024-02-25 23:46:57 +0900

    Add files via upload

---
+++ DOS/plot_layer.py
@@ -0,0 +1,48 @@
+import numpy as np
+from scipy.spatial.distance import euclidean
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from ase.io import read, write
+from ase.visualize import view
+
+import sys
+
+
+x,y,z = np.loadtxt('aldos.data', unpack = True, usecols= (0,1,2))
+x_new = (-31.96828 + x*0.06344 )*0.529177
+
+###best
+fig = plt.figure(figsize = [3.5,2.625])
+ax = fig.gca()
+ax.tick_params(labelsize='small', direction = 'in',width=1)
+ax.spines['top'].set_linewidth(1)
+ax.spines['bottom'].set_linewidth(1)
+ax.spines['right'].set_linewidth(1)
+ax.spines['left'].set_linewidth(1)
+#set font
+#plt.rcParams["mathtext.fontset"]="dejavuserif"
+font = {'family': 'serif',
+        'color':  'black',
+        'weight': 'normal',
+        'size': 12,
+        }
+
+marker_size=2
+norm_z = np.array(z)
+norm_z = (norm_z - min(norm_z)) / (max(norm_z) - min(norm_z))
+
+# Define the color range you want to visualize
+vmin = 0.0
+vmax = 0.5
+scatter = plt.scatter(y, x_new, marker_size, c=z, vmin=vmin, vmax=vmax,
+                      cmap='viridis')
+###
+ax.set_xlim(-10,5)
+ax.set_xlabel('$E - E_F$ (eV)')
+ax.set_ylabel('$z$ ($\mathrm{\AA}$)')
+ax.axvline(x = 0,linestyle='--', color = 'grey', linewidth = 0.5)
+# Set colorbar
+cbar = plt.colorbar(scatter)
+cbar.set_label('DOS')
+plt.tight_layout()
+plt.savefig('layer_dos.png', dpi=300)
